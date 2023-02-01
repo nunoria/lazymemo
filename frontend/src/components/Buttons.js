c54025bb56d834c473811daefa1380e5292c8fb4
kibum.lee <kissyou7@gmail.com>
2023-02-01 21:10:55 +0900

    네비게이션 버튼 동작 처리
    버튼 Active 색상 적용

--- frontend/src/components/Buttons.js
+++ frontend/src/components/Buttons.js
@@ -1,19 +1,22 @@
 
+export const Button = ({ name, isActive, onClickEvent }) => {
 
-export const Button = ({ name, onClickEvent }) => {
+    const activeClass = "bg-theme-gray text-white";
+    const inActiveClass = "text-theme-gray";
+
     return (
-        <div>
-
-        </div>
+        <button onClick={onClickEvent}
+            className={"px-5 py-3 font-pretandard font-bold text-sm rounded-3xl " + (isActive ? activeClass : inActiveClass)} >
+            <span>{name}</span>
+        </button>
     )
 }
 
 export const TagButton = ({ name, onClickEvent }) => {
     return (
         <button onClick={onClickEvent}
-            className="bg-white px-6 py-1 font-pretandard font-bold text-sm rounded-2xl">
+            className="bg-white px-6 py-1 font-pretandard font-bold text-sm rounded-2xl focus:bg-theme-gray focus:text-white">
             <span>{name}</span>
         </button>
     )
 }
-
--- frontend/src/components/Navi.js
+++ frontend/src/components/Navi.js
@@ -1,15 +1,30 @@
 
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
+import { Button } from './Buttons';
 
+
+const NAVI_BTN_MYURL = {
+    name: "My URL",
+    url: "/myurl"
+}
+
+const NAVI_BTN_ALLURL = {
+    name: "All URL",
+    url: "/allurl"
+}
 
 export default function Navi() {
+
+    let location = useLocation();
+    // console.log(location.pathname);
+
     return (
         <div className="bg-top flex justify-center h-[87px] py-3">
             <div className=" pbox flex flex-row items-center">
                 {/* 1/3 균등 메뉴 구성 */}
-                <div className=" basis-1/3 flex justify-start gap-4 font-bold text-sm font-pretandard">
-                    <Link to="/myurl"><span>My URL</span></Link>
-                    <Link to="/allurl"><span>All URL</span></Link>
+                <div className=" basis-1/3 flex justify-start gap-1">
+                    <Link to={NAVI_BTN_MYURL.url}><Button name={NAVI_BTN_MYURL.name}  isActive={location.pathname==NAVI_BTN_MYURL.url?true:false}/></Link>
+                    <Link to={NAVI_BTN_ALLURL.url}><Button name={NAVI_BTN_ALLURL.name} isActive={location.pathname==NAVI_BTN_ALLURL.url?true:false}/></Link>
                 </div>
                 <div className=" basis-1/3">
                     <Link to="/">
